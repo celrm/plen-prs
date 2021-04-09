9586b63b3bc755bb60dceabc2ea0a63e778ddc20
Celia Rubio <celia_rubio@hotmail.es>
2021-04-09 12:47:00 +0200

    cosas 2.2

--- Pr2 .1/script.sh
+++ Pr2 .1/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny.cup
+java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoCUP/AnalizadorSintacticoTiny -symbols AnalizadorSintacticoCUP/ClaseLexica -nopositions Tiny.cup
 cd AnalizadorSintacticoCUP/
 javac -cp "../cup.jar" */*.java
 java -cp ".:../cup.jar" asint.Main ../input.txt
--- Pr2.2/script.sh
+++ Pr2.2/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions Tiny.cup
+
+java -cp cup.jar java_cup.Main -parser AnalizadorSintacticoCUP/AnalizadorSintacticoTiny -symbols AnalizadorSintacticoCUP/ClaseLexica -nopositions Tiny.cup
+
+cd AnalizadorSintacticoCUP/
+java -cp jlex.jar JLex.Main ejemplo.l
 cp ejemplo.l.java alex/AnalizadorLexicoTiny.java
+
 javac -cp "../cup.jar" */*.java
 java -cp ".:../cup.jar" asint.Main ../input.txt
