535ae0befc5f97d08f38d7e2eecea2e43ac90f9c
mariaal <mariaarranzl@gmail.com>
2021-04-09 12:39:07 +0200

    main 1.3!

--- Pr1.3/alex/Main.java
+++ Pr1.3/alex/Main.java
@@ -7,31 +7,57 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
-   public static void main(String[] args) throws FileNotFoundException, IOException {
-     ArrayList<UnidadLexica> ps = new ArrayList<UnidadLexica>();
-     ArrayList<UnidadLexica> pt = new ArrayList<UnidadLexica>();
-     Reader input = new InputStreamReader(new FileInputStream(args[0]));
-     AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
-     UnidadLexica unidad;
-     do {
-       unidad = al.yylex();
-       System.out.println(unidad);
-       if(unidad.clase() == ClaseLexica.PALABRA)
-        ps.add(unidad);
-       else if(unidad.clase() == ClaseLexica.PATRON)
-        pt.add(unidad);
-     }
-     while (unidad.clase() != ClaseLexica.EOF);   
-     System.out.println("PALABRAS:");
-     for (UnidadLexica i : ps) {
-      System.out.println(i);
-     }
-     System.out.println("PATRONES:");
-     for (UnidadLexica i : pt) {
-         int occs = 0;
-      System.out.println(i.toString() + ": " + occs);
-     }
-    }        
-} 
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Reader input = new InputStreamReader(new FileInputStream(args[0]));
+        AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
+        UnidadLexica unidad;
+        ArrayList<UnidadLexica> patrones = new ArrayList<UnidadLexica>();
+        ArrayList<UnidadLexica> palabras = new ArrayList<UnidadLexica>();
+        do {
+            unidad = al.yylex();
+            if (unidad.clase() == ClaseLexica.PALABRA){
+                palabras.add(unidad);
+            }
+            else if (unidad.clase() == ClaseLexica.PATRON){
+                patrones.add(unidad);
+            }
+            System.out.println(unidad);
+        }
+        while (unidad.clase() != ClaseLexica.EOF);
+        System.out.println("PALABRAS");
+        Iterator<UnidadLexica> itr = palabras.iterator();
+        while(itr.hasNext()) {
+            UnidadLexica element = itr.next();
+            System.out.println(element.lexema());
+        }
+        System.out.println("PATRONES");
+        itr = patrones.iterator();
+        while(itr.hasNext()) {
+            UnidadLexica element = itr.next();
+            String lexema = element.lexema();
+            int index = lexema.indexOf('*');
+            String iz = lexema.substring(0,index);
+            String der = lexema.substring(index+1);
+            int occur = 0;
+            Iterator<UnidadLexica> itraux = palabras.iterator();
+            while(itraux.hasNext()) {
+                UnidadLexica elementaux = itraux.next();
+                String lexaux = elementaux.lexema();
+                if (lexaux.length() >= lexema.length()){
+                    String izaux = lexaux.substring(0,index);
+                    String deraux = lexaux.substring(lexaux.length()- (lexema.length()-index)+1);
+                    if (iz.compareTo(izaux)== 0 && der.compareTo(deraux)== 0){
+                        ++occur;
+                    }
+                }
+            }
+            System.out.print(lexema);
+            System.out.print( ": ");
+            System.out.println( occur);
+        }
+    }
+}
+
